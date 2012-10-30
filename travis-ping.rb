d54ba0d50cd4c4269958b906ffbb2a308427c652
Stefan Oderbolz <oderbolz@gmail.com>
2012-10-30 23:08:53 +0100

    Adapted travis-ping for new github and travis API

--- travis-ping.rb
+++ travis-ping.rb
@@ -27,48 +27,47 @@ module Travis
     # @see https://github.com/github/github-services/blob/master/lib/service/events/push_helpers.rb
     # @see https://help.github.com/articles/post-receive-hooks
     def self.forge_payload(owner_name, name)
-      last_build_id = http_get("http://travis-ci.org/#{owner_name}/#{name}.json")['last_build_id']
-      build         = http_get("http://travis-ci.org/builds/#{last_build_id}.json")
+      last_build_id = http_get("https://travis-ci.org/#{owner_name}/#{name}.json")['last_build_id']
+      build         = http_get("https://travis-ci.org/builds/#{last_build_id}.json")
       before        = build['compare_url'][/([a-f0-9]+)\.\.\./, 1]
 
-      email      = http_get("http://github.com/api/v2/json/user/show/#{owner_name}")['user']['email']
-      repository = http_get("http://github.com/api/v2/json/repos/show/#{owner_name}/#{name}")['repository']
-      commit     = http_get("http://github.com/api/v2/json/commits/show/#{owner_name}/#{name}/#{build['commit']}")['commit']
+      user       = http_get("https://api.github.com/users/#{owner_name}")
+      repository = http_get("https://api.github.com/repos/#{owner_name}/#{name}")
+      commit     = http_get("https://api.github.com/repos/#{owner_name}/#{name}/git/commits/#{build['commit']}")
       pusher     = commit['committer'].dup
-      pusher.delete 'username'
 
       repository['owner'] = {
-        'name' => repository['owner'],
+        'type'  => user['type'],
+        'login' => user['login'],
+        'email' => user['email']
       }
 
       head_commit = {
-        'added'     => commit['added'],
-        'author'    => commit['author'],
-        'committer' => commit['committer'],
-        # @todo not sure what to do about +distinct+
-        'distinct'  => true,
-        'id'        => commit['id'],
+        'sha'       => commit['id'],
         'message'   => commit['message'],
-        'modified'  => commit['modified'].map{|x| x['filename']},
-        'removed'   => commit['modified'],
-        'timestamp' => commit['authored_date'],
-        'url'       => "https://github.com#{commit['url']}",
+        'date'      => commit['authored_date'],
+        'commiter'  => { 'name' => commit['committer']['name'], 'email' => commit['committer']['email'] },
+        'author'    => { 'name' => commit['author']['name'], 'email' => commit['author']['email'] },
+        'compare'   => build['compare_url'] 
       }
-
-      { 'after'       => build['commit'],
-        # @todo +before+ will just be six characters long, not 40
+      
+      # see https://github.com/travis-ci/travis-core/blob/master/lib/travis/services/requests/receive/push.rb
+      { 
+        'after'       => build['commit'],
         'before'      => before,
-        # @todo add other commits
         'commits'     => [head_commit],
         'compare'     => build['compare_url'],
-        # @todo these are usually false, but not always
         'created'     => false,
         'deleted'     => false,
         'forced'      => false,
-        'head_commit' => head_commit,
         'pusher'      => pusher,
         'ref'         => 'refs/heads/master',
-        'repository' => repository,
+        'repository'  => {
+          'name'        => repository['name'],
+          'description' => repository['description'],
+          '_links'      => { 'html' => { 'href' => repository['url'] } },
+          'owner'       => repository['owner']
+        }
       }
     end
 
